6720e07408e1a54f000d4eff056d3b37da1a2526
Vshweta12 <shwetaverma12121998@gmail.com>
2022-08-26 14:22:01 +0000

    a

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -35,7 +35,7 @@ namespace WishList.Controllers
         {
             if (ModelState.IsValid)
                 return View(model);
-           var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
+            var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
             if(!result.Succeeded)
             {
                 foreach(var error in result.Errors)
